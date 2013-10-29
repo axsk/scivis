df131935262599d6bdd88f08a9b268c1bfc1b2ce
Julius Plenz <julius@plenz.com>
2013-10-29 20:30:18 +0100

    Trailing Space entfernen

--- src/MinJV/MinJV.java
+++ src/MinJV/MinJV.java
@@ -19,12 +19,12 @@ abstract class CB implements ActionListener {
 public class MinJV extends Applet {
     public Frame jvFrame;
     public PvViewer jvViewer;
-    
+
     public MinJV()
     {
         jvFrame = new jv.object.PsMainFrame(this, null);
         jvViewer = new PvViewer(this, jvFrame);
-        
+
         PjProject project = new PjProject("testprojekt");
         jvViewer.addProject(project);
         jvViewer.selectProject(project);
@@ -35,7 +35,7 @@ public class MinJV extends Applet {
         //frame.setSize(800, 600);
         jvFrame.setVisible(true);
     }
-    
+
     public static void main(String args[]) {
         new MinJV();
     }
