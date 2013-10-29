ac4199a499efa46e9326a753d9beae8f551a7ead
Julius Plenz <julius@plenz.com>
2013-10-29 20:22:37 +0100

    Eine Art von Callback für Buttons implementieren

--- src/sheet2/EulerCharacteristic.java
+++ src/sheet2/EulerCharacteristic.java
@@ -10,41 +10,51 @@ import jv.geom.PgElementSet;
 import jv.object.PsDebug;
 
 @SuppressWarnings("serial")
+abstract class CB implements ActionListener {
+    abstract public void run(ActionEvent ev);
+    public void actionPerformed(ActionEvent ev) {
+        run(ev);
+    }
+}
 
-public class EulerCharacteristic extends MinJV implements ActionListener {
-
+@SuppressWarnings("serial")
+public class EulerCharacteristic extends MinJV {
     public static void main(String args[]) {
         EulerCharacteristic app = new EulerCharacteristic();
     }
 
     public EulerCharacteristic() {
-        Button button = new Button("Colorize and Euler");
-        button.addActionListener(this);
+        addButton();
+    }
+
+    private void addButton() {
+        final Button button = new Button("Colorize and Euler");
+        final EulerCharacteristic outer = this;
+        button.addActionListener(new CB() {
+            public void run(ActionEvent ev) {
+                PgElementSet geo = (PgElementSet) outer.jvViewer.getCurrentProject().getGeometry();
+
+                if (geo == null)
+                return;
+
+                // Colorize Shape
+                Color colors[] = new Color[2];
+                colors[0] = Color.WHITE;
+                colors[1] = Color.BLUE;
+                for (int i = 0; i <= geo.getNumElements() - 1; i++) {
+                    geo.setElementColor(i, colors[i % 2]);
+                }
+                geo.showElementColors(true);
+                geo.update(null);
+
+                // Compute Euler Characteristic
+                int v = geo.getNumVertices();
+                int f = geo.getNumElements();
+                int e = f * 3 / 2;
+                PsDebug.message(v + " vertices, " + e + " edges, " + f + " faces. thus the euler char. is " + (v - e + f));
+            }
+        });
         this.add(button, BorderLayout.SOUTH);
         this.jvFrame.pack();
     }
-
-    public void actionPerformed(ActionEvent event) {
-        PgElementSet geo = (PgElementSet) this.jvViewer.getCurrentProject().getGeometry();
-
-        if (geo == null)
-            return;
-
-        // Colorize Shape
-        Color colors[] = new Color[2];
-        colors[0] = Color.WHITE;
-        colors[1] = Color.BLUE;
-        for (int i = 0; i <= geo.getNumElements() - 1; i++) {
-            geo.setElementColor(i, colors[i % 2]);
-        }
-        geo.showElementColors(true);
-        geo.update(null);
-
-        // Compute Euler Characteristic
-        int v = geo.getNumVertices();
-        int f = geo.getNumElements();
-        int e = f * 3 / 2;
-        PsDebug.message(v + " vertices, " + e + " edges, " + f + " faces. thus the euler char. is " + (v - e + f));
-    }
-
 }
