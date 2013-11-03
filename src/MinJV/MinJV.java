d00922136d0758aaaa37916de8525c09c1127e83
Dominik Otto <dominik.otto@gmail.com>
2013-11-03 15:01:36 +0100

    Added infopanel in MinJV

--- src/MinJV/MinJV.java
+++ src/MinJV/MinJV.java
@@ -2,15 +2,19 @@ package MinJV;
 
 import java.applet.Applet;
 import java.awt.*;
+
 import jv.loader.PgLoader;
 import jv.object.PsConfig;
+import jv.object.PsPanel;
+import jv.object.PsViewerIf;
 import jv.project.PjProject;
 import jv.viewer.PvViewer;
+import jv.project.PjProject_IP;
 
 @SuppressWarnings("serial")
 public class MinJV extends Applet {
 
-    static String DEFAULTGEO = "Cow_5804.byu";
+    static String DEFAULTGEO = "models/byu/Cow_5804.byu";
 
     public Frame jvFrame;
     public PvViewer jvViewer;
@@ -20,12 +24,16 @@ public class MinJV extends Applet {
         jvFrame = new jv.object.PsMainFrame(this, null);
         jvViewer = new PvViewer(this, jvFrame);
 
-        PjProject project = new PjProject("testprojekt");
+        PjProject project = new PjProject("this");
         jvViewer.addProject(project);
         jvViewer.selectProject(project);
-
+        
+        //project.addTitle("Torus Knot");
+        //project.add(new PsPanel());
+        //project.addLine(1);
         this.setLayout(new BorderLayout());
-        this.add(jvViewer.getDisplay().getCanvas(), BorderLayout.CENTER);
+        this.add(jvViewer.getDisplay().getCanvas(), BorderLayout.CENTER); //Add 3D-Window
+        this.add(jvViewer.getPanel(PsViewerIf.PROJECT), BorderLayout.EAST); //Add Panel
         jvFrame.pack();
         jvFrame.setVisible(true);
     }
