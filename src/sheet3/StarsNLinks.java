001f8b1293f820dcdb445fcd19789072486d7e7e
U-notebook\Alex <Alex@notebook.(none)>
2013-11-03 18:51:25 +0100

    solution to sheet3

--- src/sheet3/StarsNLinks.java
+++ src/sheet3/StarsNLinks.java
@@ -1,94 +1,113 @@
 package sheet3;
 
-
 import MinJV.*;
-import java.awt.BorderLayout;
 import java.awt.Button;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Panel;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import jv.geom.PgElementSet;
-import jv.object.PsDebug;
+import jv.object.PsObject;
 import jv.object.PsPanel;
 import jv.vecmath.PiVector;
 import jvx.geom.PgVertexStar;
 
 @SuppressWarnings("serial")
 
-public class StarsNLinks extends MinJV{
-	
-	protected Button Star = new Button("Star");
-	protected Button Link = new Button("Link");
-	protected Button fNeighbor = new Button("F-Neighbor");
+public class StarsNLinks extends MinJV {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		StarsNLinks app = new StarsNLinks();
-		if (args.length != 0) {
-	        app.loadModel(args[0]);
-	    } else {
-	        app.loadModel(null);
-	    }
+    protected Button Star = new Button("Star");
+    protected Button Link = new Button("Link");
+    protected Button fNeighbor = new Button("F-Neighbor");
 
-	}
-	
-	public StarsNLinks(){
-		PsPanel pjip = this.project.getInfoPanel();
-		pjip.addTitle("Stars 'n' Links");
-		
-		Panel losButtons = new Panel(new FlowLayout(FlowLayout.CENTER));
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        StarsNLinks app = new StarsNLinks();
+        if (args.length != 0) {
+            app.loadModel(args[0]);
+        } else {
+            app.loadModel(null);
+        }
 
-		losButtons.add(Star);
-		Star.addActionListener(this);
-		
-		losButtons.add(Link);
-		Link.addActionListener(this);
-		
-		losButtons.add(fNeighbor);
-		fNeighbor.addActionListener(this);
-		
-		pjip.add(losButtons);
-		pjip.addLine(1);
-	}
-	public void actionPerformed(ActionEvent event) {
-		Object source = event.getSource();
-		if (source == Star){
-			//PsDebug.message("I am preforming a stary action!");
-			painStar();
-		} else if(source == Link){
-			//PsDebug.message("I am preforming a linking action!");
-			paintLink();
-		} else if(source == fNeighbor){
-			paintNeighbor();
-		}
-	}
-	
-	public void painStar(){
-		//paint the star
-		PgElementSet geo = (PgElementSet) jvViewer.getCurrentProject().getGeometry();
-		int[] verts = geo.getMarkedVertices();
-		PgVertexStar star = new PgVertexStar();
-		star.makeVertexStar(geo, verts[0], 0);
-		PsDebug.message(star.toString());
-	}
-	public void paintLink(){
-		//paint the link
-		PgElementSet geo = (PgElementSet) jvViewer.getCurrentProject().getGeometry();
-		int[] verts = geo.getMarkedVertices();
-		PgVertexStar star = new PgVertexStar();
-		star.makeVertexStar(geo, verts[0], -1);
-	}
-	public void paintNeighbor(){
-		//paint the neighbor
-	}
-	
-	private PgElementSet geo = (PgElementSet) jvViewer.getCurrentProject().getGeometry();
-	//private PgVectorField selction = geo.getSelectedVectorField(); 
-	
+    }
 
+    public StarsNLinks() {
+        PsPanel pjip = this.project.getInfoPanel();
+        pjip.addTitle("Stars 'n' Links");
+
+        Panel losButtons = new Panel(new FlowLayout(FlowLayout.CENTER));
+
+        losButtons.add(Star);
+        Star.addActionListener(this);
+
+        losButtons.add(Link);
+        Link.addActionListener(this);
+
+        losButtons.add(fNeighbor);
+        fNeighbor.addActionListener(this);
+
+        pjip.add(losButtons);
+        pjip.addLine(1);
+    }
+
+    public void actionPerformed(ActionEvent event) {
+        Object source = event.getSource();
+        if (source == Star) {
+            painStar();
+        } else if (source == Link) {
+            paintLink();
+        } else if (source == fNeighbor) {
+            paintNeighbor();
+        }
+    }
+
+    public void painStar() {
+        //paint the star
+        PgElementSet geo = (PgElementSet) jvViewer.getCurrentProject().getGeometry();
+        PgVertexStar star = new PgVertexStar();
+        for (int marked : geo.getMarkedVertices()) {
+            star.makeVertexStar(geo, marked, -1);
+            for (int i : star.getElement().getEntries()) {
+                geo.setElementColor(i, Color.yellow);
+            }
+        }
+        geo.showElementColors(true);
+        geo.update(null);
+    }
+
+    public void paintLink() {
+        //paint the link     
+        PgElementSet geo = (PgElementSet) jvViewer.getCurrentProject().getGeometry();
+        PgVertexStar star = new PgVertexStar();
+        for (int marked : geo.getMarkedVertices()) {
+            star.makeVertexStar(geo, marked, -1);
+            for (int i : star.getLink().getEntries()) {
+                geo.setVertexColor(i, Color.orange);
+            }
+        }
+        geo.showVertices(true);
+        geo.showVertexColors(true);
+        geo.update(null);
+    }
+
+    public void paintNeighbor() {
+        //paint the neighbor
+        PgElementSet geo = (PgElementSet) jvViewer.getCurrentProject().getGeometry();
+        for (int i = 0; i <= geo.getNumElements() - 1; i++) {
+            if (geo.hasTagElement(i, PsObject.IS_SELECTED)) {
+                PiVector neighs = geo.getNeighbour(i);
+                for (int neigh : neighs.getEntries()) {
+                    geo.setElementColor(neigh, Color.GREEN);
+                }
+                geo.setElementColor(i, Color.GREEN);
+            }
+        }
+        geo.showElementColors(true);
+        geo.update(null);
+    }
+
+    private PgElementSet geo;
 }
