55a4f0aebdd07235eff486e23827bb3bb68d383e
U-notebook\Alex <Alex@notebook.(none)>
2013-11-04 18:45:02 +0100

    mainly cosmetics

--- src/sheet3/StarsNLinks.java
+++ src/sheet3/StarsNLinks.java
@@ -1,6 +1,5 @@
 package sheet3;
 
-
 import MinJV.*;
 import java.awt.Button;
 import java.awt.Color;
@@ -11,115 +10,109 @@ import java.awt.event.ActionEvent;
 import jv.geom.PgElementSet;
 import jv.object.PsObject;
 import jv.object.PsPanel;
-import jv.vecmath.PiVector;
 import jvx.geom.PgVertexStar;
 
-@SuppressWarnings("serial")
+//@SuppressWarnings("serial")
 
-public class StarsNLinks extends MinJV{
-	
-	protected Button Star = new Button("Star");
-	protected Button Link = new Button("Link");
-	protected Button fNeighbor = new Button("F-Neighbor");
+public class StarsNLinks extends MinJV {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		StarsNLinks app = new StarsNLinks();
-		if (args.length != 0) {
-	        app.loadModel(args[0]);
-	    } else {
-	        app.loadModel(null);
-	    }
+    protected Button bStar = new Button("Star");
+    protected Button bLink = new Button("Link");
+    protected Button bFNeighbor = new Button("F-Neighbor");
 
-	}
-	
-	public StarsNLinks(){
-		PsPanel pjip = this.project.getInfoPanel();
-		pjip.addTitle("Stars 'n' Links");
-		
-		Panel losButtons = new Panel(new FlowLayout(FlowLayout.CENTER));
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        StarsNLinks app = new StarsNLinks();
+        if (args.length != 0) {
+            app.loadModel(args[0]);
+        } else {
+            app.loadModel(null);
+        }
 
-		losButtons.add(Star);
-		Star.addActionListener(this);
-		
-		losButtons.add(Link);
-		Link.addActionListener(this);
-		
-		losButtons.add(fNeighbor);
-		fNeighbor.addActionListener(this);
-		
-		pjip.add(losButtons);
-		pjip.addLine(1);
-	}
-	public void actionPerformed(ActionEvent event) {
-		Object source = event.getSource();
-		if (source == Star){
-			//PsDebug.message("I am preforming a stary action!");
-			painStar();
-		} else if(source == Link){
-			//PsDebug.message("I am preforming a linking action!");
-			paintLink();
-		} else if(source == fNeighbor){
-			paintNeighbor();
-		}
-	}
-	
-	public void painStar(){
-		PgElementSet geo = (PgElementSet) jvViewer.getCurrentProject().getGeometry();
-		int[] verts = geo.getMarkedVertices();
-		PgVertexStar star = new PgVertexStar();
-		
-		for(int v:verts){
-			//getting the star
-			star.makeVertexStar(geo, v, -1);
-			int[] stare = star.getElement().getEntries();
-			
-			//painting the star
-	        for (int vec: stare) {
-	             geo.setElementColor(vec, Color.YELLOW);
-	        }
-		}
-		
-		geo.showElementColors(true);
+    }
+
+    public StarsNLinks() {
+        PsPanel pjip = this.project.getInfoPanel();
+        pjip.addTitle("Stars 'n' Links");
+
+        Panel losButtons = new Panel(new FlowLayout(FlowLayout.CENTER));
+
+        losButtons.add(bStar);
+        bStar.addActionListener(this);
+
+        losButtons.add(bLink);
+        bLink.addActionListener(this);
+
+        losButtons.add(bFNeighbor);
+        bFNeighbor.addActionListener(this);
+
+        pjip.add(losButtons);
+        pjip.addLine(1);
+    }
+
+    public void actionPerformed(ActionEvent event) {
+        Object source = event.getSource();
+        if (source == bStar) {
+            paintStar();
+        } else if (source == bLink) {
+            paintLink();
+        } else if (source == bFNeighbor) {
+            paintNeighbor();
+        }
+    }
+
+    public void paintStar() {
+        PgElementSet geo = (PgElementSet) jvViewer.getCurrentProject().getGeometry();
+        PgVertexStar star = new PgVertexStar();
+
+        for (int vert : geo.getMarkedVertices()) {
+            //calculate star
+            star.makeVertexStar(geo, vert, -1);
+            //paint the star
+            for (int face : star.getElement().getEntries()) {
+                geo.setElementColor(face, Color.YELLOW);
+            }
+        }
+
+        geo.showElementColors(true);
         geo.update(null);
-	}
-	public void paintLink(){
-		PgElementSet geo = (PgElementSet) jvViewer.getCurrentProject().getGeometry();
-		int[] verts = geo.getMarkedVertices();
-		PgVertexStar star = new PgVertexStar();
-		
-		for(int v:verts){
-			//getting the star
-			star.makeVertexStar(geo, v, -1);
-			int[] link = star.getLink().getEntries();
-			
-			//painting the star
-	        for (int vec: link) {
-	             geo.setVertexColor(vec, Color.YELLOW);
-	        }
-		}
-		geo.showVertices(true);
-		geo.showVertexColors(true);
+    }
+
+    public void paintLink() {
+        PgElementSet geo = (PgElementSet) jvViewer.getCurrentProject().getGeometry();
+        PgVertexStar star = new PgVertexStar();
+
+        for (int vert : geo.getMarkedVertices()) {
+            //calculate link
+            star.makeVertexStar(geo, vert, -1);
+            //paint the link
+            for (int link : star.getLink().getEntries()) {
+                geo.setVertexColor(link, Color.YELLOW);
+            }
+        }
+        geo.showVertices(true);
+        geo.showVertexColors(true);
         geo.update(null);
-	}
-	public void paintNeighbor(){
-		PgElementSet geo = (PgElementSet) jvViewer.getCurrentProject().getGeometry();
-		for (int i = 0; i < geo.getNumElements(); i++){
-				if(geo.hasTagElement(i, PsObject.IS_SELECTED)){
-					geo.setElementColor(i, Color.GREEN);
-					PiVector neig = geo.getNeighbour(i);
-					for(int k: neig.getEntries()){
-						geo.setElementColor(k, Color.GREEN);
-					}
-				}
-		}
-		
-		geo.showElementColors(true);
+    }
+
+    public void paintNeighbor() {
+        PgElementSet geo = (PgElementSet) jvViewer.getCurrentProject().getGeometry();
+
+        for (int elem = 0; elem < geo.getNumElements(); elem++) {
+            if (geo.hasTagElement(elem, PsObject.IS_SELECTED)) {
+                //if selected, color the face
+                geo.setElementColor(elem, Color.GREEN);
+                //and its neighbours
+                for (int neighb : geo.getNeighbour(elem).getEntries()) {
+                    geo.setElementColor(neighb, Color.GREEN);
+                }
+            }
+        }
+
+        geo.showElementColors(true);
         geo.update(null);
-		
-	}
-	
+    }
 
 }
