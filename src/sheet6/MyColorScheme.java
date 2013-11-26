05af29657b910852abea44c59c228bb3f65f125f
U-notebook\Alex <smartmind23@googlemail.com>
2013-11-26 17:45:19 +0100

    inserted levellines

--- src/sheet6/MyColorScheme.java
+++ src/sheet6/MyColorScheme.java
@@ -1,6 +1,7 @@
 package sheet6;
 
 import jv.number.PuComplex;
+import java.lang.Math;
 
 public class MyColorScheme implements ColorScheme {
 
@@ -12,9 +13,15 @@ public class MyColorScheme implements ColorScheme {
 		 * format as in java.awt.Color.getRGB().
 		 */
                 
-		
+		//int max = (z.arg()%(3.14/4)<(3.14/32))?0.5:0;
+                
+            float rax = ((5*z.abs())-Math.floor(5*z.abs()) < 0.1d) ? 0.7f : 1f;
 		// This is just an example how to ensure that the resulting color has full (=opaque) alpha channel.
-	    return java.awt.Color.HSBtoRGB((float)(z.arg()/(2*3.14)), (float)1., (float)(-1/(z.abs()+1)+1));
+	    //return java.awt.Color.HSBtoRGB((float)(z.arg()/(2*java.lang.Math.PI)),1f, ax);
+            float alx = (((z.arg()/(Math.PI/4))-Math.floor(z.arg()/(Math.PI/4)))*(Math.PI/4) < Math.PI/200) ? 0.7f : 1f;
+            return java.awt.Color.HSBtoRGB((float)(z.arg()/(2*java.lang.Math.PI)),rax, (float)(-1/(z.abs()+1)+1)*alx);
+
+            
 	}
 
 }
